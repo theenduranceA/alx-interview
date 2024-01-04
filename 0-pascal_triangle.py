7926fb6c6f3ab0484b4877106dba6ab4a0bc6ac9
theenduranceA <theenduranceaneke@gmail.com>
2024-01-04 01:23:01 +0000

    Task 0

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,4 +1,11 @@
+#!/usr/bin python3
+""" Script for Pascal's triangle."""
+
+
 def pascal_triangle(n):
+    """ Function that returns a list of integers,
+    representing the Pascal’s triangle of n. """
+
     if n <= 0:
         return []
 
