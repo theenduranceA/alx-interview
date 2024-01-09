1370343e4f469d0e61a42c7a9d318ae8b9515953
theenduranceA <theenduranceaneke@gmail.com>
2024-01-09 13:40:13 -0800

    Task done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """ A script for lockboxes."""
 
-from collections import deque
-
 
 def canUnlockAll(boxes):
     """ Function that checks if all boxes can be opened."""
     my_boxes = set()
-    queue = deque([0])
 
-    while queue:
-        current_box = queue.popleft()
+    def dfs(current_box):
         my_boxes.add(current_box)
 
         for key in boxes[current_box]:
             if key not in my_boxes:
-                queue.append(key)
+                dfs(key)
+    dfs(0)
 
     return len(my_boxes) == len(boxes)
