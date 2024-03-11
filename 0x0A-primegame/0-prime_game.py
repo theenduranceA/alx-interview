5ba91693fb7940cc6243a9e52966343c03d121c5
theenduranceA <theenduranceaneke@gmail.com>
2024-03-11 10:01:36 -0700

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python4
+""" A script for Prime Game. """
+
+
+def isWinner(x, nums):
+    """Function to determine the winner in a prime game between two players."""
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def count_primes_up_to_n(n):
+        count = sum(1 for i in range(2, n + 1) if is_prime(i))
+        return count
+
+    maria_wins, ben_wins = 0, 0
+
+    for n in nums:
+        prime_count = count_primes_up_to_n(n)
+        ben_wins += prime_count % 2 == 0
+        maria_wins += prime_count % 2 == 1
+
+    if maria_wins == ben_wins:
+        return None
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
